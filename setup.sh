d4dff50c73760ba73b10337f925229ed0c71a7d5
BigAnteater <walterjamesarron@gmail.com>
2022-05-14 11:34:31 -0700

    Moved the 'setup' variable to AFTER it moves the setup folder

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-# This program doesn't do much but set up my startup scrippoo
+# This program doesn't do much but set up my startup scripts
 
 # Creates the itExists variable. This is for testing if .profile exists. 1 = yes, 0 = no
 itExists=0
+
+# Moves startup folder into ~ folder and makes it hidden.
+mv startup/ .startup/ && cp -r .startup/ ~/ 
+
 # Creates the startup variable. I only used this so that I don't have to type out so much stuff.
 startup=bash ~/.startup/
-
-mv startup/ .startup/ && cp -r .startup/ ~/ # Moves startup folder into ~ folder and makes it hidden.
 
 # Checks if .profile is existing. If not, $exists will stay at 0. If it does exist, then it changes it to 1.
 test -r ~/.profile && itExists=1
