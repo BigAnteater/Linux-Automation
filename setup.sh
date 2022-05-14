ba9d53d6a9936ad9ed92b77c2f0829639a027078
BigAnteater <walterjamesarron@gmail.com>
2022-05-14 11:05:52 -0700

    IDK

--- setup.sh
+++ setup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# This program doesn't do much but set up my startup scripts
-echo 
+# This program doesn't do much but set up my startup scrippoo
 
 # Creates the itExists variable. This is for testing if .profile exists. 1 = yes, 0 = no
 itExists=0
 # Creates the startup variable. I only used this so that I don't have to type out so much stuff.
 startup=bash ~/.startup/
 
-mv startup/ .startup/ && cp -r startup/ ~/ # Moves startup folder into ~ folder and makes it hidden.
+mv startup/ .startup/ && cp -r .startup/ ~/ # Moves startup folder into ~ folder and makes it hidden.
 
 # Checks if .profile is existing. If not, $exists will stay at 0. If it does exist, then it changes it to 1.
-test -r ~/.profile && $itExists=1
+test -r ~/.profile && itExists=1
 
 # if it doesn't exist, it will create .profile.
 if [ $itExists != 1 ]
